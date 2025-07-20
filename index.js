d7a47d9f2236d5dedd7e9d3bdff77f5a3ba6df3f
CarlosBF <cborrellf@outlook.es>
2025-07-20 20:17:51 +0200

    Made use of body-parser with autMiddleware

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const cors = require('cors')
 const dbConnection = require('./config/db')
 const productRoutes = require('./routes/productRoutes')
 const userRoutes = require('./routes/userRoutes')
+const authMidleware = require('./middlewares/authMiddleware')
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+authMidleware.setupAPP(app)
 
 app.use(cors())
 app.use(express.json())
